214df6043685622c0d0f5691bd932f42987974b3
fsoc106 <72858650+fsoc106@users.noreply.github.com>
2023-03-30 10:46:27 +0200

    Update flemme.sh

--- flemme.sh
+++ flemme.sh
@@ -1,3 +1,4 @@
 apt install -y nano
 wget https://github.com/praetorian-inc/Hob0Rules/raw/master/wordlists/rockyou.txt.gz
 gzip -d rockyou.txt.gz
+wget https://github.com/fsoc106/vast/raw/main/sorted.rule
